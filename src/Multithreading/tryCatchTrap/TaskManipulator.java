350306c530d9136c2052c4ca4c4e5bcfc1542499
Andrey <bursov@web.de>
2022-12-04 21:36:05 +0100

    Try-Catch-Trap task complete

--- src/Multithreading/tryCatchTrap/TaskManipulator.java
+++ src/Multithreading/tryCatchTrap/TaskManipulator.java
@@ -1,7 +1,32 @@
 package Multithreading.tryCatchTrap;
 
-public class TaskManipulator implements Runnable {
+public class TaskManipulator implements Runnable, CustomThreadManipulator {
+    private Thread thread;
+
     @Override
     public void run() {
+        // To check and work with data it is better not to use a constantly changing variable thread.
+        // Better use the static method of the Thread instead.
+        Thread.yield();
+        while (!Thread.currentThread().isInterrupted()){
+            System.out.println(Thread.currentThread().getName());
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException ignore) {
+                Thread.currentThread().interrupt();
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void start(String threadName) {
+        thread = new Thread(this, threadName);
+        thread.start();
+    }
+
+    @Override
+    public void stop() {
+        thread.interrupt();
     }
 }
